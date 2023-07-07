0a336e7871c728ec0b01630925b4b1c4993cf1b7
[SW5]최형욱 <guddnr888@gmail.com>
2023-07-07 15:31:09 +0900

    "[Rename branch] 브랜치명 변경하기 위한 push"

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 require("dotenv").config();
 
 const express = require("express");
+const path = require("path");
+const cors = require("cors");
 
 const app = express();
 app.set("view engine", "ejs");
@@ -9,8 +11,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors());
 app.use(express.static(path.join(__dirname, "public")));
+
 app.get("/", function (req, res) {
-    res.render("index");
+    res.render("signin.ejs");
 });
 
 app.use((req, res, next) => {
@@ -31,3 +34,4 @@ app.listen(PORT, () => {
         `정상적으로 커스텀잇 서버를 시작하였습니다.  http://localhost:${PORT}`
     );
 });
+app.set("views", path.join(__dirname, "views")); 
