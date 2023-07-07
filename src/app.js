8cb4f7520c17dcbb43de730924d120392d0d3888
[SW5] 이남경 <namkyeong1008@gmail.com>
2023-07-07 22:17:38 +0900

    file upload

--- src/app.js
+++ src/app.js
@@ -1,16 +1,17 @@
 require("dotenv").config();
 
 const express = require("express");
-
+const path = require("path");
+const cors = require("cors");
 const app = express();
 app.set("view engine", "ejs");
-
+app.set("views", path.join(__dirname, "views"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors());
 app.use(express.static(path.join(__dirname, "public")));
 app.get("/", function (req, res) {
-    res.render("index");
+    res.render("order-list.ejs");
 });
 
 app.use((req, res, next) => {
@@ -27,7 +28,5 @@ app.use((err, req, res, next) => {
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log(
-        `정상적으로 커스텀잇 서버를 시작하였습니다.  http://localhost:${PORT}`
-    );
+    console.log(`정상적으로 커스텀잇 서버를 시작하였습니다.  http://localhost:${PORT}`);
 });
