5aff6a83fbe318ab2b273bf4467cd8502466f5ba
[SW5]최형욱 <guddnr888@gmail.com>
2023-07-08 02:36:25 +0900

    "design: header,footer,signin 수정"

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 require("dotenv").config();
 
 const express = require("express");
+const path = require("path");
+const cors = require("cors");
 
 const app = express();
 app.set("view engine", "ejs");
@@ -9,8 +11,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors());
 app.use(express.static(path.join(__dirname, "public")));
+
 app.get("/", function (req, res) {
-    res.render("order-list.ejs");
+    res.render("signin.ejs");
 });
 
 app.use((req, res, next) => {
@@ -27,5 +30,7 @@ app.use((err, req, res, next) => {
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log(`정상적으로 커스텀잇 서버를 시작하였습니다.  http://localhost:${PORT}`);
+    console.log(
+        `정상적으로 커스텀잇 서버를 시작하였습니다.  http://localhost:${PORT}`
+    );
 });
