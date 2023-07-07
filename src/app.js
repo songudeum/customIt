e7e9a15acf523b6987a8466724f7ef5425c92bc0
[SW5] 박수현 <pshyeon0707@gmail.com>
2023-07-07 18:34:21 +0900

    Feat: admin-aside

--- src/app.js
+++ src/app.js
@@ -1,16 +1,18 @@
 require("dotenv").config();
 
 const express = require("express");
-
+const path = require("path");
+const cors = require("cors");
 const app = express();
 app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors());
 app.use(express.static(path.join(__dirname, "public")));
 app.get("/", function (req, res) {
-    res.render("index");
+    res.render("admin-category.ejs");
 });
 
 app.use((req, res, next) => {
