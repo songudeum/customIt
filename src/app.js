31488684d4e8f4273515a9c12bbd02c7b4ed1d83
[SW5]최형욱 <guddnr888@gmail.com>
2023-07-09 00:00:01 +0900

    "refactor: path 삭제"

--- src/app.js
+++ src/app.js
@@ -1,21 +1,23 @@
-require("dotenv").config();
+require('dotenv').config();
 
-const express = require("express");
+const express = require('express');
+const path = require('node:path');
+const cors = require('cors');
 
 const app = express();
-app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "views"));
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
-    res.render('admin-aside.ejs');
+    res.render('admin-signin.ejs');
 });
 
 app.use((req, res, next) => {
-    const error = new Error("Resource Not Found");
+    const error = new Error('Resource Not Found');
     error.statusCode = 404;
     next(error);
 });
