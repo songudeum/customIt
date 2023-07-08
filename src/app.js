bb7bdb68908646d5356c99227a30ef0cee0b585e
[SW5]최형욱 <guddnr888@gmail.com>
2023-07-08 23:45:58 +0900

    "merge: 병합 시도"

--- src/app.js
+++ src/app.js
@@ -13,7 +13,7 @@ app.use(cors());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
-    res.render('admin-signin.ejs');
+    res.render('admin-aside.ejs');
 });
 
 app.use((req, res, next) => {
