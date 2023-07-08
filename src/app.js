a20043f67a236c49d7c6d529ed337f8bf8b9648c
[SW5] 이남경 <namkyeong1008@gmail.com>
2023-07-08 16:03:14 +0900

    file upload

--- src/app.js
+++ src/app.js
@@ -1,7 +1,8 @@
 require("dotenv").config();
 
 const express = require("express");
-
+const path = require("node:path");
+const cors = require("cors");
 const app = express();
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
