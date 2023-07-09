bcd8a7faf1de4fd472efeda83894362dc3676c4e
Kimsuyeon <songudeum@gmail.com>
2023-07-09 23:59:43 +0900

    feat: 회원 탈퇴 및 로그아웃 구현

--- src/routes/api/users.js
+++ src/routes/api/users.js
@@ -2,6 +2,8 @@ const { Router } = require('express');
 const { Users } = require('../../data-access');
 const asyncHandler = require('../../utils/async-handler');
 const createHash = require('../../utils/hash-password');
+const getUserFromJWT = require('../../middlewares/get-user-from-jwt');
+const { jwtVerify } = require('../../utils/jwt');
 
 const router = Router();
 
@@ -12,16 +14,21 @@ router.post(
         const { email } = req.body;
         const emailDuplicate = await Users.findOne({ email });
         if (emailDuplicate) {
+            res.status(200);
             res.send('중복된 이메일이 존재합니다.');
-        } else res.send('성공');
+        } else {
+            res.status(200);
+            res.send('성공');
+        }
     }),
 );
 
 // 개인페이지 사용자 정보 수정 api(send 나중에 render로 수정)
 router.put(
     '/info/edit/:email',
+    getUserFromJWT,
     asyncHandler(async (req, res) => {
-        const userEmail = req.params.email;
+        const userEmail = jwtVerify(req);
         const { email, password, newPassword, name, phoneNumber, address } = req.body;
         const user = await Users.findOne({ email: userEmail });
         const userPw = user.password;
@@ -41,6 +48,7 @@ router.put(
                 address,
             },
         );
+        res.status(200).json({ message: '사용자 정보 완료' });
         res.send(newUserInfo);
     }),
 );
