ca568b152ee187902bfc650bb0c9b4173d776c9b
[SW5]최형욱 <guddnr888@gmail.com>
2023-07-10 09:21:56 +0900

    "design:detail-page.css 수정"

--- src/app.js
+++ src/app.js
@@ -13,7 +13,7 @@ app.use(cors());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
-    res.render('main-cart.ejs');
+    res.render('detail-page.ejs');
 });
 
 app.use((req, res, next) => {
