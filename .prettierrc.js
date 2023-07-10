ada058e97067b75e70d957402f3f22e56ab9f21d
Kimsuyeon <songudeum@gmail.com>
2023-07-10 16:28:40 +0900

    fix: merge충돌 해결

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,29 +1,14 @@
 module.exports = {
-<<<<<<< HEAD
     printWidth: 100,
     tabWidth: 4,
     useTabs: false,
     semi: true,
     singleQuote: true,
-    quoteProps: "as-needed",
-    trailingComma: "all",
+    quoteProps: 'as-needed',
+    trailingComma: 'all',
     bracketSpacing: true,
     bracketSameLine: false,
-    arrowParens: "always",
-    htmlWhitespaceSensitivity: "css",
+    arrowParens: 'always',
+    htmlWhitespaceSensitivity: 'css',
     singleAttributePerLine: false,
-=======
-  printWidth: 100,
-  tabWidth: 4,
-  useTabs: false,
-  semi: true,
-  singleQuote: true,
-  quoteProps: 'as-needed',
-  trailingComma: 'all',
-  bracketSpacing: true,
-  bracketSameLine: false,
-  arrowParens: 'always',
-  htmlWhitespaceSensitivity: 'css',
-  singleAttributePerLine: false,
->>>>>>> 128b67a41cbcd655e39b12fe2caba3d1ef1ab7a9
 };
