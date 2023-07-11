ee0970289e57835386ffefc6476a6c98c2a09acf
[SW5] 이남경 <namkyeong1008@gmail.com>
2023-07-11 12:07:01 +0900

    Design: empty-orderlist css 수정

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,18 +1,4 @@
 module.exports = {
-<<<<<<< HEAD
-    printWidth: 100,
-    tabWidth: 4,
-    useTabs: false,
-    semi: true,
-    singleQuote: true,
-    quoteProps: "as-needed",
-    trailingComma: "all",
-    bracketSpacing: true,
-    bracketSameLine: false,
-    arrowParens: "always",
-    htmlWhitespaceSensitivity: "css",
-    singleAttributePerLine: false,
-=======
   printWidth: 100,
   tabWidth: 4,
   useTabs: false,
@@ -25,5 +11,4 @@ module.exports = {
   arrowParens: 'always',
   htmlWhitespaceSensitivity: 'css',
   singleAttributePerLine: false,
->>>>>>> 128b67a41cbcd655e39b12fe2caba3d1ef1ab7a9
 };
--- src/app.js
+++ src/app.js
@@ -44,6 +44,9 @@ const app = express();
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
+app.get('/', (req, res) => {
+    res.render('empty-order-list.ejs');
+});
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors());
