d380b78eb8ad0ba1953d011cebc72efaadb732bb
[SW5] 신혜정 <shj3229123@gmail.com>
2023-07-11 14:46:51 +0900

    feat: 로컬스토리지 데이터, 장바구니 라우터, 장바구니 서비스 로직 구현중

---
+++ src/routes/cart.js
@@ -0,0 +1,20 @@
+const { Router } = require('express');
+const { getProductsFromLocalStorage } = require('../services/cart');
+
+const asyncHandler = require('../utils/async-handler');
+
+const router = Router();
+
+// 로컬스토리지에 담긴 모든 상품 정보를 가져와 장바구니 페이지를 렌더링하는 라우터
+router.get(
+    '/cart',
+    asyncHandler(async (req, res) => {
+        const products = await getProductsFromLocalStorage();
+        // getProductsFromLocalStorage() 함수를 사용해 로컬 스토리지에 저장된 상품 정보를 가져옴
+
+        res.render('cart', { products });
+        // products 값을 넘겨주어, 이를 사용하여 장바구니 페이지를 렌더
+    }),
+);
+
+module.exports = router;
