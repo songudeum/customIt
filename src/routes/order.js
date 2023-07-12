65a2157da4a9d17e0eef96f8da4863ff87497efc
[SW5] 신혜정 <shj3229123@gmail.com>
2023-07-12 11:07:49 +0900

    feat: 라우터 주소 작성

--- src/routes/order.js
+++ src/routes/order.js
@@ -11,7 +11,7 @@ router.get(
     asyncHandler(async (req, res) => {
         // 로컬스토리지에서 카트 데이터 가져오기
         const cartData = JSON.parse(getCartDataFromLocalStorage());
-        res.render('장바구니 내역을 담은 결제 전 주문서 페이지', { cartData });
+        res.render('수정예정', { cartData });
     }),
 );
 
@@ -34,17 +34,7 @@ router.get(
             };
         });
 
-        res.render('마이페이지 주문 리스트', { orderList });
-    }),
-);
-
-// 마이페이지 주문내역 상세
-router.get(
-    '/:id/orderList/:orderId',
-    asyncHandler(async (req, res) => {
-        const { orderId } = req.params;
-        const order = await Order.findById(orderId);
-        res.render('마이페이지 주문서 상세보기', { order });
+        res.render('order-list', { orderList });
     }),
 );
 
@@ -54,7 +44,7 @@ router.get(
     asyncHandler(async (req, res) => {
         const { orderId } = req.params;
         const order = await Order.findById(orderId);
-        res.render('마이페이지 주문서 상세보기', { order });
+        res.render('order-detail', { order });
     }),
 );
 
@@ -66,17 +56,17 @@ router.get(
 
         const order = await Order.findOne({ orderId });
 
-        res.render('배송정보 수정 페이지', { order });
+        res.render('order-list-edit', { order });
     }),
 );
 
 // 결제완료
 router.get('/payment', (req, res) => {
-    res.render('결제 완료 페이지');
+    res.render('order-complete');
 });
 
 // 주문 취소 완료 페이지
 router.get('/cancel', (req, res) => {
-    res.render('취소 완료 페이지');
+    res.render('order-cancel');
 });
 module.exports = router;
