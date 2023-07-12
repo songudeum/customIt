63968586abe66d74d8dbc6862bd4ede8629e4388
Kimsuyeon <songudeum@gmail.com>
2023-07-12 23:33:41 +0900

    fix: 스키마 조회불가 수정

--- src/passport/strategies/local.js
+++ src/passport/strategies/local.js
@@ -9,12 +9,13 @@ const local = new LocalStrategy(config, async (email, password, done) => {
     try {
         // 입력된 이메일을 바탕으로 유저 찾고 이메일과 비밀번호 일치 여부 확인
         const user = await Users.findOne({ email });
-        const userPw = user.password
+        
         if (!user) {
             const error = new Error('이메일 또는 비밀번호가 일치하지 않습니다.');
             error.statusCode = 400;
             throw error;
-        }
+        }   
+        const userPw = user.password
         if (!comparePassword(password, userPw)) {
             const error = new Error('이메일 또는 비밀번호가 일치하지 않습니다.');
             error.statusCode = 400;
