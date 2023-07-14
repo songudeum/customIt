da5c6ef02262cbc7989bf11e217249f12f4389bb
Kimsuyeon <songudeum@gmail.com>
2023-07-14 13:19:03 +0900

    refactor: jwt 복호화  함수 require로 불러오는 것으로 변경

--- src/middlewares/admin-check.js
+++ src/middlewares/admin-check.js
@@ -1,12 +1,9 @@
-const jwt = require('jsonwebtoken');
-const { secret } = require('../utils/jwt');
+const { jwtVerify } = require('../utils/jwt');
 
 // jwt토큰 확인 미들웨어
 function adminCheck(req, res, next) {
-    const { token } = req.cookies;
-
     // jwt 토큰 복호화 한 후의 유저의 email값
-    const verified = jwt.verify(token, secret).email;
+    const verified = jwtVerify(req);
 
     // email에서 @이후의 주소만 추출
     const email = verified.split('@')[1];
@@ -16,7 +13,7 @@ function adminCheck(req, res, next) {
         throw error;
     }
 
-    res.status(200).json({message: "토큰 확인 완료"})
+    res.status(200).json({ message: '토큰 확인 완료' });
     next();
 }
 
